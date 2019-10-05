c68b950f0f3f63e7aa3f041ac01598b6cb4a2be0
chikechris <cchike2017@gmail.com>
2019-10-05 11:55:43 -0400

    built out Form.js file

--- src/components/ExpForm.js
+++ src/components/ExpForm.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import { MdSend } from 'react-icons/md'
 
 const ExpForm = () => {
   return (
-    <div>
-      ExpForm
-    </div>
+    <form>
+      <div className='form-center'>
+        <div className='form-group'>
+          <label htmlFor='expense'>charge</label>
+          <input
+            type='text'
+            className='form-control'
+            id='charge'
+            name='charge'
+            placeholder='input charge here......'
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='expense'>amount</label>
+          <input
+            type='text'
+            className='form-control'
+            id='amount'
+            name='amount'
+            placeholder='input amount here......'
+          />
+        </div>
+      </div>
+      <button type='submit' className='btn'>
+        submit
+        <MdSend/>
+      </button>
+    </form>
   )
 }
 
